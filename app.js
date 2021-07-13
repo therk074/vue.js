67dfbaa05dfce09a742caab83484089562b10847
[therk] <[mju5906105378@mju.ac.th]>
2021-07-13 08:48:53 +0700

    main

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
 new Vue({ //สร้าง object
     el:"#vue01", //environment
     data:{ //ส่วนที่จะนำไปแสดง
-        message:"Hello World" //ตัวแปร
-    }
+        name:"thekkao", //ตัวแปร
+        job:"full stack developer",
+        age:23, 
+    },
+    methods: { //function ทำงานกับ object
+        getName:function(){
+            return this.name
+        },
+        setName:function(n){
+            return this.name = n //รับค่ามาเปลี่ยนชื่อตัวแปร
+        }
+    },
 })
