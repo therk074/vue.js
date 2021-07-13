d2922afa3650ca6a70d034fd2e770c8d8e12389f
[therk] <[mju5906105378@mju.ac.th]>
2021-07-14 01:00:34 +0700

    main

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 new Vue({ //สร้าง object
-    el:"#vue01", //environment
+    el:"#vue01", //environment จะเรียกไปใช้งานที่ id ไหน
     data:{ //ส่วนที่จะนำไปแสดง
         name:"thekkao", //ตัวแปร
         job:"full stack developer",
         age:23, 
+        web:'https://github.com/therk074/vue.js'
     },
     methods: { //function ทำงานกับ object
         getName:function(){
