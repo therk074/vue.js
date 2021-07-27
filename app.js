f1e6735464264b10e81af1d042c99c6df91de921
[therk] <[mju5906105378@mju.ac.th]>
2021-07-27 11:23:53 +0700

    v-if / button

--- app.js
+++ app.js
@@ -1,18 +1,9 @@
 new Vue({ //สร้าง object
-    el:"#vue01", //environment จะเรียกไปใช้งานที่ id ไหน
-    data:{ //ส่วนที่จะนำไปแสดง
-        name:"thekkao", //ตัวแปร
-        job:"full stack developer",
-        age:23, 
-        web:'https://github.com/therk074/vue.js',
-        girlgroups:[],
-        dinner:'',
-        pet:''
+    el: "#vue01", //environment จะเรียกไปใช้งานที่ id ไหน
+    data: { //ส่วนที่จะนำไปแสดง
+        like: false
     },
     methods: {
-        addCount:function(){
-            this.count++;
-            console.log(this.count);
-        }
+
     },
 })
