676e47ebe41600b6e2c1db53dec5bd717bfe651f
[therk] <[mju5906105378@mju.ac.th]>
2021-08-02 08:58:03 +0700

    v-for array

--- app.js
+++ app.js
@@ -1,24 +1,9 @@
 new Vue({
     el: "#vue01",
-    data: { 
-        data_array: ['array1','array2','array3','array4'],
-        data2:[
-            {
-                name:'aurora',
-                edu:{
-                    university:'มช'
-                },
-                age:24,
-                job:'singer'
-            },
-            {
-                name:'guy',
-                edu:{
-                    university:'มจ'
-                },
-                age:88,
-                job:'CEO'
-            }
-        ]
-    } 
+    data: {
+        newData: {
+            empName: '',
+            empSalary: 0
+        }
+    }
 })
