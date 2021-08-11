a9702651118e97996ec913e660b0daa87924cab1
[therk] <[mju5906105378@mju.ac.th]>
2021-08-11 12:02:12 +0700

    ใช้ form push เข้า data

--- app.js
+++ app.js
@@ -4,6 +4,27 @@ new Vue({
         newData: {
             empName: '',
             empSalary: 0
+        },
+        empData: [
+            {
+                empName: 'ทดสอบ',
+                empSalary: 1000
+            },
+            {
+                empName: 'ไม่ทดสอบ',
+                empSalary: 2000
+            }
+        ]
+    },
+    methods: {
+        AddEmployee: function(){
+            this.empData.push({ // push value จาก this.ชื่อdata.ชื่อinput
+                empName: this.newData.empName,
+                empSalary: this.newData.empSalary
+            });
+            // เมื่อ push เสร็จ ให้รีเซ็ตเป็น
+            this.newData.empName = '';
+            this.newData.empSalary = 0;
         }
-    }
+    },
 })
