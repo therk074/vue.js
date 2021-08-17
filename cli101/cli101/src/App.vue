c6b15a97927c8bdf251abaeb6cd7ec2595a18bb0
therk <mju5906105378@mju.ac.th>
2021-08-17 17:19:26 +0700

    input รูปแล้วดูรูปที่ใส่

--- cli101/cli101/src/App.vue
+++ cli101/cli101/src/App.vue
@@ -1,18 +1,14 @@
 <template>
   <div id="app">
     <div class="container">
-      <h4 style="text-align: center">Limit Message</h4>
-      <hr />
-      <div class="input-group mb-3">
-        <input
-          type="text"
-          class="form-control"
-          v-model="message"
-          :maxlength="max"
-        />
-        <div class="input-group-prepend">
-          <span class="input-group-text" v-text="max-message.length">20</span>
-        </div>
+      <br />
+      <div v-if="!image">
+        <input type="file" class="form-control" @change="onFileChange" />
+      </div>
+      <div v-else>
+        <img :src="image" class="img-fluid" />
+        <br /><br />
+        <button class="btn btn-danger" @click="removeImage">cancel</button>
       </div>
     </div>
   </div>
@@ -23,9 +19,26 @@ export default {
   name: "App",
   data() {
     return {
-      message: "",
-      max: 20,
+      image: "",
     };
+  },
+  methods: {
+    onFileChange: function (event) {
+      var files = event.target.files;
+      if (!files.length) return;
+      this.createImage(files[0]);
+    },
+    createImage(files) {
+      var reader = new FileReader();
+      reader.onload = (event) => {
+        this.image = event.target.result;
+      };
+      reader.readAsDataURL(files);
+      console.log(this.image);
+    },
+    removeImage() {
+      this.image = "";
+    },
   },
 };
 </script>
