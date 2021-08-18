5c409d8c145fb9b6df5486e05d06845d8bc2e6c5
therk <mju5906105378@mju.ac.th>
2021-08-18 15:03:59 +0700

    checked item and plus total value

--- cli101/cli101/src/App.vue
+++ cli101/cli101/src/App.vue
@@ -1,14 +1,21 @@
 <template>
   <div id="app">
     <div class="container">
-      <br />
-      <div v-if="!image">
-        <input type="file" class="form-control" @change="onFileChange" />
-      </div>
-      <div v-else>
-        <img :src="image" class="img-fluid" />
-        <br /><br />
-        <button class="btn btn-danger" @click="removeImage">cancel</button>
+      <div class="row">
+        <div align="center">menu</div>
+        <div class="col-md-3" v-for="(prod, index) in products" :key="index">
+          <p>
+            product name : <b>{{ prod.name }}</b>
+          </p>
+          <p>
+            price : <b>{{ prod.price }}</b>
+          </p>
+          <img :src="prod.img" style="width: 50%" />
+          <input type="checkbox" class="form-check-input" @change="chk(prod)" />
+        </div>
+        <div class="col-md-4" style="text-align: right">
+          total {{ total() }} baht
+        </div>
       </div>
     </div>
   </div>
@@ -19,25 +26,35 @@ export default {
   name: "App",
   data() {
     return {
-      image: "",
+      products: [
+        {
+          name: "bottle of water",
+          price: 7,
+          img: "https://static.bigc.co.th/media/catalog/product/8/8/8851952350147.jpg",
+          active: false,
+        },
+        {
+          name: "a cup of tea",
+          price: 35,
+          img: "https://lh3.googleusercontent.com/proxy/mPAnLE0QNq6ByCPMlrhzfkoO504urkXxQXGFNq0Z0VX0mWXPR8t5MF9YaUguHfCa3zZQaAaKSyP4vj3KLTgL3VDsepMFdYhGOEl-GQbSyIV18lqmhXrnlQ",
+          active: false,
+        },
+      ],
     };
   },
   methods: {
-    onFileChange: function (event) {
-      var files = event.target.files;
-      if (!files.length) return;
-      this.createImage(files[0]);
+    chk: function (item) {
+      item.active = !item.active;
     },
-    createImage(files) {
-      var reader = new FileReader();
-      reader.onload = (event) => {
-        this.image = event.target.result;
-      };
-      reader.readAsDataURL(files);
-      console.log(this.image);
-    },
-    removeImage() {
-      this.image = "";
+    total: function () {
+      var sum = 0;
+      this.products.forEach(function (item) {
+        if (item.active) {
+          //ถ้าเป็น boolean ถ้าจริงมันก็เข้า if เลย
+          sum += item.price;
+        }
+      });
+      return sum;
     },
   },
 };
