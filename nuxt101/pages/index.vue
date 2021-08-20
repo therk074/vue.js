6732622bcdbf957894efc3921edbde87a9f47e43
therk <mju5906105378@mju.ac.th>
2021-08-20 14:12:26 +0700

    nuxt-link

--- nuxt101/pages/index.vue
+++ nuxt101/pages/index.vue
@@ -1,7 +1,11 @@
 <template>
-  <Tutorial/>
+  <div>
+    <nuxt-link to="/users">users</nuxt-link>
+    <nuxt-link to="/admin">admin</nuxt-link>
+  </div>
+  <!-- a href ด้วย nuxt-link ให้ไปที่ path ที่เราต้องการ -->
 </template>
 
 <script>
-export default {}
+export default {};
 </script>
