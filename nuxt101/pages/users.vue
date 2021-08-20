82bc97f3cf6ccddbad5c32927466f2d83956543c
therk <mju5906105378@mju.ac.th>
2021-08-20 14:54:47 +0700

    nested routes / nuxt-child

---
+++ nuxt101/pages/users.vue
@@ -0,0 +1,13 @@
+<template>
+  <!-- *จาก /user.vue ข้างนอก* ให้ users นี้ อิงจากไฟล์ users/index.vue ทำงานสัมพันธ์กัน มันจะเอาด้านนอกสุดมาแสดงก่อน-->
+  <!-- การกำหนด route ซ้อนกัน -->
+  <div>
+    <h3>users : external</h3>
+    <hr />
+    <form action="">
+      <input type="text" />
+      <input type="submit" value="insert" />
+    </form>
+    <nuxt-child /> <!-- ชื่อจะต้องเหมือนกัน ตัวไฟล์ชื่อ users.vue ด้านนอกที่เป็น parent ถึงจะไปเรียก users/index.vue ด้านใน -->
+  </div>
+</template>
--- nuxt101/pages/users/index.vue
+++ nuxt101/pages/users/index.vue
@@ -1,3 +1,23 @@
 <template>
-    <div>this is user page</div>
+  <!-- *จาก user/index.vue ข้างใน* -->
+  <div>
+    <h3>users : internal</h3>
+    <h4>table</h4>
+    <hr />
+    <table class="table">
+      <thead>
+        <tr>
+          <th>name</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>tsuneko</td>
+        </tr>
+        <tr>
+          <td>nestuko</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
 </template>
