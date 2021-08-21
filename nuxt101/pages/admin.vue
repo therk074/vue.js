00fec7b15f4b8ecd7453f3e10210f9c05ac07c80
[therk] <[mju5906105378@mju.ac.th]>
2021-08-21 23:44:51 +0700

    main nest to layouts

---
+++ nuxt101/pages/admin.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <div>
+      <h2>Form User</h2>
+      <form action="">
+        <input type="text" />
+        <input type="submit" value="insert" />
+      </form>
+      <nuxt-child />
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  layout: "coreLayout",
+};
+</script>
